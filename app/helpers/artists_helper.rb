8fdb61b617fea961e20f05eba312b146040dda2a
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-31 17:02:38 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -5,9 +5,9 @@ module ArtistsHelper
 
   def artist_select(song, artist)
     if artist
-      
+      text_field_tag "song[:artist_id]"
     else
-      select_tag "song[:artist]", options_from_collection_for_select(Artist.all, :id, :name)
+      select_tag "song[:artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     end
   end
 end
